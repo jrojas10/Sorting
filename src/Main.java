c3267492ac45772d635e59648ef7447b14139bda
jrojas10 <jrojasbf@gmail.com>
2017-05-10 10:46:14 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,19 @@
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list10 = new ArrayList<Integer>();
-		Random r = new Random();
-		for (int i = 0; i < 10; i++) {
-			int d = r.nextInt();
-			System.out.println(d);
-			list10.add(d);
-
-		}
-
-		// ArrayList<Long> list100 = new ArrayList<Long>();
-		// for (int i = 0; i < 100; i++) {
-		// Long d = r.nextLong() * 100;
-		// System.out.println(d);
-		// list100.add(d);
-		//
-		// }
-
-		System.out.println();
 
 		Sorting s = new Sorting();
-		Integer[] bubbleList10 = s.toArray(list10);
-		// Integer[] insertionList10 = s.toArray(list10);
-		// s.bubbleSort(bubbleList10);
-		// s.print(bubbleList10);
-		System.out.println("Bubble Sort");
-		long nanoStart = System.nanoTime();
-
-		s.improvedBubbleSort(bubbleList10);
-		long nanoEnd = System.nanoTime();
-
-		System.out.println("Bubble sort time = " + (nanoEnd - nanoStart) + " nano seconds");
-
-		s.print(bubbleList10);
-		//long miliseconds = System.currentTimeMillis();
-		// long seconds = TimeUnit.MILLISECONDS.toSeconds(miliseconds);
+	
+		s.createList(1000); // creates unsorted array
+	//	s.improvedBubbleSort(); //sorts and prints swaps and compares
+		//s.insertionSort();
+		s.mergeSortFinal();
+	
 
 		System.exit(0);
 	}
--- src/Sorting.java
+++ src/Sorting.java
@@ -1,55 +1,127 @@
 import java.util.ArrayList;
+import java.util.Random;
 
-public class Sorting {
+public class Sorting<E> {
+	Long[] array;
 
 	public Sorting() {
 
 	}
 
-	protected Integer[] toArray(ArrayList<?> list) {
-		Integer[] array = list.toArray(new Integer[list.size()]);
+	protected Long[] createList(int size) {
+		ArrayList<Long> list = new ArrayList<Long>();
+		Random r = new Random();
+		for (int i = 0; i < size; i++) {
+			Long random = (long) r.nextInt();
+			list.add(random);
+
+		}
+		return toArray(list);
+	}
+
+	private Long[] toArray(ArrayList<?> list) {
+		Long[] array2 = list.toArray(new Long[list.size()]);
 		// Double[] array = new Double[list.size()];
 		// array = (Double[]) list.toArray(array);
+		this.array = array2;
 		return array;
 
 	}
 
-	protected void print(Integer[] array) {
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+	protected void print() {
+		for (int i = 0; i < this.array.length; i++) {
+			System.out.println(this.array[i]);
 		}
 	}
 
-//	protected void bubbleSort(Integer[] array) {
-//		for (int k = 1; k < array.length; k++) {
-//			for (int i = 0; i < array.length - k; i++) {
-//				if (array[i] > array[i + 1]) {
-//					int temp = array[i + 1];
-//					array[i + 1] = array[i];
-//					array[i] = temp;
-//				}
-//			}
-//		}
-//
-//	}
-
-	protected void improvedBubbleSort(Integer[] array) {
+	protected void improvedBubbleSort() {
+		long nanoStart = System.nanoTime();
 		int swapCounter = 0;
 		int compareCounter = 0;
 		boolean pass = true;
-		for (int k = 1; k < array.length && pass; k++) {
+		for (int k = 1; k < this.array.length && pass; k++) {
 			pass = false;
-			for (int i = 0; i < array.length - k; i++) {
+			for (int i = 0; i < this.array.length - k; i++) {
 				compareCounter++;
-				if (array[i] > array[i + 1]) {
-					int temp = array[i];
-					array[i] = array[i + 1];
-					array[i + 1] = temp;
+				if (this.array[i] > this.array[i + 1]) {
+					Long temp = this.array[i];
+					this.array[i] = this.array[i + 1];
+					this.array[i + 1] = temp;
 					swapCounter++;
 					pass = true;
 				}
 			}
 		}
+		long nanoEnd = System.nanoTime();
+		print();
+
 		System.out.println("Compare counter = " + compareCounter + "\nSwap Counter = " + swapCounter);
+		System.out.println("Bubble sort time = " + (nanoEnd - nanoStart) + " nano seconds");
+	}
+
+	protected void insertionSort() {
+		long nanoStart = System.nanoTime();
+		int swapCounter = 0;
+		int compareCounter = 0;
+
+		for (int i = 1; i < this.array.length; i++) {
+			Long current = this.array[i];
+			int k;
+			for (k = i - 1; k >= 0 && this.array[k] > current; k--) {
+				compareCounter++;
+				this.array[k + 1] = this.array[k];
+				swapCounter++;
+			}
+			this.array[k + 1] = current;
+			swapCounter++;
+		}
+		long nanoEnd = System.nanoTime();
+		print();
+		System.out.println("Compare counter = " + compareCounter + "\nSwap Counter = " + swapCounter);
+		System.out.println("Insertion sort time = " + (nanoEnd - nanoStart) + " nano seconds");
+	}
+
+	protected void mergeSortFinal() {
+		long nanoStart = System.nanoTime();
+		mergeSort(this.array);
+		long nanoEnd = System.nanoTime();
+		print();
+		System.out.println("Merge sort time = " + (nanoEnd - nanoStart) + " nano seconds");
+	}
+
+	private void mergeSort(Long[] arrayH) {
+
+		int swapCounter = 0;
+		int compareCounter = 0;
+		if (arrayH.length > 1) {
+			Long[] firstHalf = new Long[arrayH.length / 2];
+			System.arraycopy(arrayH, 0, firstHalf, 0, arrayH.length / 2);
+			mergeSort(firstHalf);
+
+			int secondHalfLength = arrayH.length - arrayH.length / 2;
+			Long[] secondHalf = new Long[secondHalfLength];
+			System.arraycopy(arrayH, arrayH.length / 2, secondHalf, 0, secondHalfLength);
+			mergeSort(secondHalf);
+			merge(firstHalf, secondHalf, arrayH);
+		}
+
+	}
+
+	private void merge(Long[] first, Long[] secondHalf, Long[] array2) {
+		int curr1 = 0;
+		int curr2 = 0;
+		int curr3 = 0;
+		while (curr1 < first.length && curr2 < secondHalf.length) {
+			if (first[curr1] < secondHalf[curr2])
+				array2[curr3++] = first[curr1++];
+			else
+				array2[curr3++] = secondHalf[curr2++];
+		}
+		while (curr1 < first.length)
+			array2[curr3++] = first[curr1++];
+
+		while (curr2 < secondHalf.length)
+			array2[curr3++] = secondHalf[curr2++];
+
 	}
 }
