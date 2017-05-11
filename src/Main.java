5832b9ff8ae1f5566bd40390b2984041228f5e19
jrojas10 <jrojasbf@gmail.com>
2017-05-10 22:58:24 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,161 @@
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list10 = new ArrayList<Integer>();
-		Random r = new Random();
-		for (int i = 0; i < 10; i++) {
-			int d = r.nextInt();
-			System.out.println(d);
-			list10.add(d);
-
-		}
-
-		// ArrayList<Long> list100 = new ArrayList<Long>();
-		// for (int i = 0; i < 100; i++) {
-		// Long d = r.nextLong() * 100;
-		// System.out.println(d);
-		// list100.add(d);
-		//
-		// }
-
-		System.out.println();
-
 		Sorting s = new Sorting();
-		Integer[] bubbleList10 = s.toArray(list10);
-		// Integer[] insertionList10 = s.toArray(list10);
-		// s.bubbleSort(bubbleList10);
-		// s.print(bubbleList10);
-		System.out.println("Bubble Sort");
-		long nanoStart = System.nanoTime();
+		
+		s.createList(10);//array size 10		
+		System.out.println("Array of size 10");
+		
 
-		s.improvedBubbleSort(bubbleList10);
-		long nanoEnd = System.nanoTime();
+		System.out.println("\n Bubble sort \n");
+		s.improvedBubbleSort();
+		
+		System.out.println("\n Insertion sort \n");
+		s.insertionSort();
+		
+		System.out.println("\n Selection sort \n");
+		s.selectionSort();
+		
+		System.out.println("\n Merge sort \n");
+		s.mergeSortFinal();
+		
+		System.out.println("\n Quick sort \n");
+		s.quickSortHelper();
+		
+		System.out.println("\n Gnome sort \n");
+		s.gnomeSort();
+		
+		System.out.println("\n\n\n");
 
-		System.out.println("Bubble sort time = " + (nanoEnd - nanoStart) + " nano seconds");
-
-		s.print(bubbleList10);
-		//long miliseconds = System.currentTimeMillis();
-		// long seconds = TimeUnit.MILLISECONDS.toSeconds(miliseconds);
-
-		System.exit(0);
+		
+		
+		s.createList(100); // array size 100
+		System.out.println("Array of size 100\n\n");
+		
+		
+		System.out.println("\n Bubble sort \n");
+		s.improvedBubbleSort();
+		
+		System.out.println("\n Insertion sort \n");
+		s.insertionSort();
+		
+		System.out.println("\n Selection sort \n");
+		s.selectionSort();
+		
+		System.out.println("\n Merge sort \n");
+		s.mergeSortFinal();
+		
+		System.out.println("\n Quick sort \n");
+		s.quickSortHelper();
+		
+		System.out.println("\n Gnome sort \n");
+		s.gnomeSort();
+		
+		System.out.println("\n\n\n");
+		
+		
+		
+		s.createList(1000); // array size 1000
+		System.out.println("Array of size 1000\n\n");
+		
+		
+		System.out.println("\n Bubble sort \n");
+		s.improvedBubbleSort();
+		
+		System.out.println("\n Insertion sort \n");
+		s.insertionSort();
+		
+		System.out.println("\n Selection sort \n");
+		s.selectionSort();
+		
+		System.out.println("\n Merge sort \n");
+		s.mergeSortFinal();
+		
+		System.out.println("\n Quick sort \n");
+		s.quickSortHelper();
+		
+		System.out.println("\n Gnome sort \n");
+		s.gnomeSort();
+		
+		System.out.println("\n\n\n");
+		
+		
+		
+		s.createList(10000); // array size 10000
+		System.out.println("Array of size 10000\n\n");
+		
+		
+		System.out.println("\n Bubble sort \n");
+		s.improvedBubbleSort();
+		
+		System.out.println("\n Insertion sort \n");
+		s.insertionSort();
+		
+		System.out.println("\n Selection sort \n");
+		s.selectionSort();
+		
+		System.out.println("\n Merge sort \n");
+		s.mergeSortFinal();
+		
+		System.out.println("\n Quick sort \n");
+		s.quickSortHelper();
+		
+		System.out.println("\n Gnome sort \n");
+		s.gnomeSort();
+		
+		System.out.println("\n\n\n");
+		
+		
+		
+		
+		s.createList(100000); // array size 100000
+		System.out.println("Array of size 100000\n\n");
+		
+		
+		System.out.println("\n Bubble sort \n");
+		s.improvedBubbleSort();
+		
+		System.out.println("\n Insertion sort \n");
+		s.insertionSort();
+		
+		System.out.println("\n Selection sort \n");
+		s.selectionSort();
+		
+		System.out.println("\n Merge sort \n");
+		s.mergeSortFinal();
+		
+		System.out.println("\n Quick sort \n");
+		s.quickSortHelper();
+		
+		System.out.println("\n Gnome sort \n");
+		s.gnomeSort();
+		
+		System.out.println("\n\n\n");
+		
+		
+		s.createList(1000000); // array size 100000
+		System.out.println("Array of size 1000000\n\n");
+		
+		
+		System.out.println("\n Bubble sort \n");
+		s.improvedBubbleSort();
+		
+		System.out.println("\n Insertion sort \n");
+		s.insertionSort();
+		
+		System.out.println("\n Selection sort \n");
+		s.selectionSort();
+		
+		System.out.println("\n Merge sort \n");
+		s.mergeSortFinal();
+		
+		System.out.println("\n Quick sort \n");
+		s.quickSortHelper();
+		
+		System.out.println("\n Gnome sort \n");
+		s.gnomeSort();
+		
+		System.out.println("\n\n\n");
 	}
 
 }
